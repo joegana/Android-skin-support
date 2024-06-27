d8a97a30d41a31ac43e4fb4c9dba1d17d871ceb8
Justin <joegana@126.com>
2024-06-27 16:40:48 +0800

    Skinable.java 标记为可继承的

--- androidx/skin-support/src/main/java/skin/support/annotation/Skinable.java
+++ androidx/skin-support/src/main/java/skin/support/annotation/Skinable.java
@@ -1,11 +1,13 @@
 package skin.support.annotation;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+@Inherited
 @Retention(RUNTIME)
 @Target({TYPE})
 public @interface Skinable {
